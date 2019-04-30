02cd9d584cff6d6ffc9b227dfab1aee0d2c42dbf
rchampa <rchampa@telecoming.com>
2019-04-30 15:20:02 +0200

    checkpoint

--- app/src/main/java/es/rchampa/inapppurchases/MainActivity.kt
+++ app/src/main/java/es/rchampa/inapppurchases/MainActivity.kt
@@ -1,10 +1,11 @@
 package es.rchampa.inapppurchases
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.android.billingclient.api.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener, PurchasesUpdatedListener {
 
@@ -12,12 +13,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, PurchasesUpdated
     private lateinit var productsAdapter : ProductsAdapter
 
     private val skuList = listOf("candy.strawberry", "candy.lemon", "android.test.purchased", "android.test.canceled", "android.test.item_unavailable")
+    private val skuSubs = listOf("subs.candies.basic", "subs.candies.all", "candy.lemon", "android.test.purchased", "android.test.canceled", "android.test.item_unavailable")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         bt_load_products.setOnClickListener(this)
+        bt_retrieve_purchases.setOnClickListener(this)
+        bt_load_subs.setOnClickListener(this)
 
         setupBillingClient()
     }
@@ -52,7 +56,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, PurchasesUpdated
         view?.let {
             when(view.id){
                 R.id.bt_load_products -> loadProducts()
-                R.id.bt_test -> testPurchase()
+                R.id.bt_retrieve_purchases -> retrievePurchase()
+                R.id.bt_load_subs -> loadSubs()
             }
         }
 
@@ -93,12 +98,45 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, PurchasesUpdated
         }
     }
 
-    private fun testPurchase(){
-//        val billingFlowParams = BillingFlowParams
-//            .newBuilder()
-//            .setSku()
-//            .build()
-//        billingClient.launchBillingFlow(this, billingFlowParams)
+    private fun loadSubs(){
+        if (billingClient.isReady) {
+            val params = SkuDetailsParams
+                .newBuilder()
+                .setSkusList(skuSubs)
+                .setType(BillingClient.SkuType.SUBS)
+                .build()
+
+            billingClient.querySkuDetailsAsync(params) { responseCode, skuDetailsList ->
+                if (responseCode == BillingClient.BillingResponse.OK) {
+                    println("BILLING | querySkuDetailsAsync, responseCode: $responseCode")
+                    initProductAdapter(skuDetailsList)
+                } else {
+                    println("BILLING | Can't querySkuDetailsAsync, responseCode: $responseCode")
+                }
+            }
+        }
+        else {
+            println("BILLING | Billing Client not ready")
+        }
+    }
+
+    private fun retrievePurchase(){
+
+        val purchasesResult = HashSet<Purchase>()
+        var result = billingClient.queryPurchases(BillingClient.SkuType.INAPP)
+        println("BILLING | queryPurchasesAsync INAPP results: ${result?.purchasesList}")
+        result?.purchasesList?.apply { purchasesResult.addAll(this) }
+        if (isSubscriptionSupported()) {
+            result = billingClient.queryPurchases(BillingClient.SkuType.SUBS)
+            result?.purchasesList?.apply { purchasesResult.addAll(this) }
+            println("BILLING | queryPurchasesAsync SUBS results: ${result?.purchasesList}")
+        }
+    }
+
+    private fun isSubscriptionSupported(): Boolean {
+        val responseCode =
+            billingClient.isFeatureSupported(BillingClient.FeatureType.SUBSCRIPTIONS)
+        return responseCode == BillingClient.BillingResponse.OK
     }
 
 }
